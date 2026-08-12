42e76356f97e82f23ed81c51d86f127ca1df9c73
Hashem Abdelati <abdelati.hashem@gmail.com>
2026-08-13 01:31:46 +0300

    Update WhatsApp contact number

--- lib/contact.ts
+++ lib/contact.ts
@@ -1,7 +1,7 @@
 export const contactEmail = "abdelati.hashem@gmail.com";
 export const contactPhone = "+962798509111";
 export const contactPhoneDisplay = "+962 7 9850 9111";
-export const whatsappPhone = contactPhone.replace(/^\+/, "");
+export const whatsappPhone = "12023558027";
 
 export function whatsappHref(message: string) {
   return `https://wa.me/${whatsappPhone}?text=${encodeURIComponent(message)}`;
